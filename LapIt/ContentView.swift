7ac58535f672609ea9fab07739da935948ba32b7
YordanMarkov <yordanmarkov2004@gmail.com>
2023-01-10 23:57:48 +0200

    Register1View added. Some logic  as well. 10.01.2023

--- LapIt/ContentView.swift
+++ LapIt/ContentView.swift
@@ -52,32 +52,162 @@ struct LogIn: View {
                     // Nothing for now
                 },
                 label: {
-                    Text("Log in")
-                        .frame(width: 100 , height: 30, alignment: .center)
-                })
-            .buttonStyle(.borderedProminent)
-            .position(x: 205, y: 540)
-            .foregroundColor(.white)
-            .tint(.init(cgColor: UIColor(red: 0, green: 0.686, blue: 0.678, alpha: 1).cgColor))
-            
-            Button(
-                action: {
-                    // Nothing for now
-                },
-                label: {
                     Text("Don't have an accout?\nRegister here.")
                         .multilineTextAlignment(.center)
                         .underline()
                 })
             .buttonStyle(.plain)
-            .position(x: 205, y: 600)
+            .position(x: 205, y: 540)
+            
+            if email.isEmpty || password.isEmpty {
+                Text("Fields are required!")
+                    .position(x: 205, y: 370)
+                    .foregroundColor(.red)
+            } else {
+                Button(
+                    action: {
+                        // Nothing for now
+                    },
+                    label: {
+                        Text("Log in")
+                            .frame(width: 100 , height: 30, alignment: .center)
+                    })
+                .buttonStyle(.borderedProminent)
+                .position(x: 205, y: 600)
+                .foregroundColor(.white)
+                .tint(.init(cgColor: UIColor(red: 0, green: 0.686, blue: 0.678, alpha: 1).cgColor))
+            }
+        }
+    }
+}
+
+struct Register1View: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var repeatPassword: String = ""
+    @State private var secured1: Bool = true
+    @State private var secured2: Bool = true
+    var body: some View {
+        ZStack {
+            Color.init(cgColor: UIColor(red: 0.568, green: 0.817, blue: 0.814, alpha: 1).cgColor).ignoresSafeArea()
+            Text("Setting up")
+                .padding()
+                .background(Color(cgColor: UIColor(red: 0, green: 0.686, blue: 0.678, alpha: 1).cgColor))
+                .font(.largeTitle)
+                .cornerRadius(10)
+                .foregroundColor(.white)
+                .frame(width: 400)
+                .position(x: 205, y: 100)
+            
+            TextField("E-mail", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 300, height: 300)
+                .position(x: 205, y: 313)
+            
+            if secured1 {
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300, height: 300)
+                    .position(x: 205, y: 375)
+            } else {
+                TextField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300, height: 300)
+                    .position(x: 205, y: 375)
+            }
+            
+            Button( // URL: https://stackoverflow.com/questions/63095851/show-hide-password-how-can-i-add-this-feature
+                action: {
+                    secured1.toggle()
+                }) {
+                    Image(systemName: self.secured1 ? "eye.slash" : "eye")
+                        .accentColor(.black)
+            }
+                .position(x: 380, y: 375)
+            
+            if secured2 {
+                SecureField("Repeat Password", text: $repeatPassword)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300, height: 300)
+                    .position(x: 205, y: 437)
+            } else {
+                TextField("Repeat Password", text: $repeatPassword)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300, height: 300)
+                    .position(x: 205, y: 437)
+            }
+            
+            Button( // URL: https://stackoverflow.com/questions/63095851/show-hide-password-how-can-i-add-this-feature
+                action: {
+                    secured2.toggle()
+                }) {
+                    Image(systemName: self.secured2 ? "eye.slash" : "eye")
+                        .accentColor(.black)
+            }
+                .position(x: 380, y: 437)
+            
+            if repeatPassword != password {
+                Text("The passwords must match!")
+                    .position(x: 205, y: 480)
+                    .foregroundColor(.red)
+            } else if password.count < 8 && !password.isEmpty && !repeatPassword.isEmpty {
+                Text("The password must be at least 8 characters!")
+                    .position(x: 205, y: 480)
+                    .foregroundColor(.red)
+            }
+            
+            if email.isEmpty || password.isEmpty || repeatPassword.isEmpty {
+                Text("Fields are required!")
+                    .position(x: 205, y: 275)
+                    .foregroundColor(.red)
+            }
+            
+            // Navigator
+            ZStack {
+                Rectangle()
+                    .fill(Color(cgColor: UIColor(red: 0, green: 0.098, blue: 0.659, alpha: 1).cgColor))
+                    .frame(width: 450, height: 21)
+                    .position(x: 205, y: 700)
+                
+                Button(
+                    action: {
+                        // Nothing for now
+                    },
+                    label: {
+                        Text(">>")
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .bold()
+                    })
+                .buttonStyle(.plain)
+                .position(x: 350, y: 700)
+                
+                Button(
+                    action: {
+                        // Nothing for now
+                    },
+                    label: {
+                        Text("<<")
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .bold()
+                    })
+                .buttonStyle(.plain)
+                .position(x: 65, y: 700)
+                
+                Image("LapItLogo")
+                    .resizable()
+                    .frame(width: 109, height: 87)
+                    .position(x: 205, y: 700)
+            }
         }
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        LogIn()
+        //LogIn()
+        Register1View()
     }
 }
     
